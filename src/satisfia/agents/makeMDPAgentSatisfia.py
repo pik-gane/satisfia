bfcb3a6eafc59c9b0f10e136bbe2b7c3cd09a155
heitzig <heitzig@pik-potsdam.de>
2024-03-06 18:05:54 +0100

    fixed for now by replacing 'steadfast' aspiration propagation from state to action by linear rescaling.

--- src/satisfia/agents/makeMDPAgentSatisfia.py
+++ src/satisfia/agents/makeMDPAgentSatisfia.py
@@ -201,6 +201,15 @@ class AspirationAgent(ABC):
 	def estAspiration4action(self, state, action, aleph4state):
 		if DEBUG:
 			print(pad(state),"| | estAspiration4action, state",prettyState(state),"action",action,"aleph4state",aleph4state,"...")
+
+		res = interpolate(self.minAdmissibleQ(state, action),
+						  relativePosition(self.minAdmissibleV(state), aleph4state, self.maxAdmissibleV(state)),
+						  self.maxAdmissibleQ(state, action));
+		if VERBOSE or DEBUG:
+			print(pad(state),"| | ╰ estAspiration4action, state",prettyState(state),"action",action,"aleph4state",aleph4state,":",res,"(rescaled)") 
+		return res
+
+		# DISABLED:
 		phi = self.admissibility4action(state, action)
 		if isSubsetOf(phi, aleph4state):
 			if VERBOSE or DEBUG:
@@ -273,7 +282,7 @@ class AspirationAgent(ABC):
 		# independent from the local policy that we are about to construct,
 		actions = self.possible_actions(state)
 		assert actions != []
-		estAlephs1 = [self.estAspiration4action(state, action, aleph4state) for action in actions]
+		estAlephs = [self.estAspiration4action(state, action, aleph4state) for action in actions]
 
 		# Estimate losses based on this estimated aspiration intervals
 		# and use it to construct softmin propensities (probability weights) for choosing actions.
@@ -301,7 +310,7 @@ class AspirationAgent(ABC):
 				p[key] = weight
 
 		indices = list(range(len(actions)))
-		propensities = propensity(indices, estAlephs1)
+		propensities = propensity(indices, estAlephs)
 
 		if DEBUG:
 			print(pad(state),"| localPolicyData", prettyState(state), aleph, actions, propensities)
@@ -309,7 +318,7 @@ class AspirationAgent(ABC):
 		for i1, p1 in distribution.categorical(indices, propensities).categories():
 			# Get admissibility interval for the first action.
 			a1 = actions[i1]
-			adm1Lo, adm1Hi = adm1 = self.admissibility4action(state, a1)
+			adm1 = self.admissibility4action(state, a1)
 
 			# If a1's admissibility interval is completely contained in aleph4state, we are done:
 			if Interval(adm1) <= Interval(aleph4state):
@@ -320,55 +329,17 @@ class AspirationAgent(ABC):
 				# For the second action, restrict actions so that the the midpoint of aleph4state can be mixed from
 				# those of estAlephs4action of the first and second action:
 				midTarget = midpoint(aleph4state)
-				estAleph1 = estAlephs1[i1]
-				mid1 = midpoint(estAleph1)
-				indices2 = [index for index in indices if between(midTarget, midpoint(estAlephs1[index]), mid1)]
+				aleph1 = estAlephs[i1]
+				mid1 = midpoint(aleph1)
+				indices2 = [index for index in indices if between(midTarget, midpoint(estAlephs[index]), mid1)]
 
-				# Since we are already set on giving a1 a considerable weight, we no longer aim to have aleph(a2)
-				# as close as possible to aleph(s), but to a target aleph that would allow mixing a1 and a2 
-				# in roughly equal proportions, i.e., we aim to have aleph(a2) as close as possible to
-				# aleph(s) + (aleph(s) - aleph(a1)):
-				aleph2target = interpolate(estAleph1, 2.0, aleph4state)
-				# Due to the new target aleph, we have to recompute the estimated alephs and resulting losses and propensities:
-				estAlephs2 = [self.estAspiration4action(state, actions[index], aleph2target) for index in indices]
-				propensities2 = propensity(indices2, estAlephs2)
-
-				if DEBUG:
-					print(pad(state),"| localPolicyData", prettyState(state), aleph4state, {"a1": a1, "midTarget": midTarget, "estAleph1": estAleph1, "mid1": mid1, "indices2": indices2, "aleph2target": aleph2target, "estAlephs2": estAlephs2, "propensities2": propensities2})
-
-				for i2, p2 in distribution.categorical(indices2, propensities2).categories():
+				for i2, p2 in distribution.categorical(indices2, [propensities[i] for i in indices2]).categories():
 					# Get admissibility interval for the second action.
 					a2 = actions[i2]
-					adm2Lo, adm2Hi = adm2 = self.admissibility4action(state, a2)
-
-					# Now we need to find two aspiration intervals aleph1 in adm1 and aleph2 in adm2,
-					# and a probability p such that
-					#	 aleph1:p:aleph2 is contained in aleph4state
-					# and aleph1, aleph2 are close to the estimates we used above in estimating loss.
-					# Instead of optimizing this, we use the following heuristic:
-					# We first choose p so that the midpoints mix exactly:
-					estAleph2 = estAlephs2[i2]
-					mid2 = midpoint(estAleph2)
+					adm2 = self.admissibility4action(state, a2)
+					aleph2 = estAlephs[i2]
+					mid2 = midpoint(aleph2)
 					p = relativePosition(mid1, midTarget, mid2)
-					# TODO try clipping? not going to work - sometimes I have print(mid1, midTarget, mid2)
-					# 3.0 0.40000000000000036 0.8000000000000007
-
-					# Now we find the largest relative size of aleph1 and aleph2
-					# so that their mixture is still contained in aleph4state:
-					# we want aleph1Lo:p:aleph2Lo >= alephLo and aleph1Hi:p:aleph2Hi <= alephHi
-					# where aleph1Lo = mid1 - x * w1, aleph1Hi = mid1 + x * w1,
-					#		 aleph2Lo = mid2 - x * w2, aleph2Hi = mid2 + x * w2,
-					# hence midTarget - x * w1:p:w2 >= alephLo and midTarget + x * w1:p:w2 <= alephHi,
-					# i.e., x <= (midTarget - alephLo) / (w1:p:w2) and x <= (alephHi - midTarget) / (w1:p:w2):
-					w1 = estAleph1[1] - estAleph1[0]
-					w2 = estAleph2[1] - estAleph2[0]
-					w = interpolate(w1, p, w2)
-					x = min((midTarget - alephLo) / w, (alephHi - midTarget) / w) if (w > 0) else 0
-					aleph1 = Interval(mid1 - x * w1, mid1 + x * w1)
-					aleph2 = Interval(mid2 - x * w2, mid2 + x * w2)
-
-					if DEBUG:
-						print(pad(state),"| localPolicyData",prettyState(state), aleph4state, {"a1": a1, "estAleph1": estAleph1, "adm1": adm1, "w1": w1, "a2": a2, "estAleph2": estAleph2, "adm2": adm2, "w2": w2, "p": p, "w": w, "x": x, "aleph1": aleph1, "aleph2": aleph2})
 
 					if VERBOSE or DEBUG:
 						print(pad(state),"| localPolicyData, state",prettyState(state),"aleph4state",aleph4state,": a1,p,a2",a1,p,a2,"adm12",adm1,adm2,"aleph12",aleph1,aleph2)
