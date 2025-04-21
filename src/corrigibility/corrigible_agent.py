ef8cf9d3e2084df82967f12650475985b330f745
heitzig <heitzig@pik-potsdam.de>
2025-04-21 11:56:24 +0200

    corr

--- src/corrigibility/corrigible_agent.py
+++ src/corrigibility/corrigible_agent.py
@@ -22,7 +22,7 @@ class CorrigibleAgent:
         assert boltzmann_temperature >= 0.0
         self.boltzmann_temperature = boltzmann_temperature
 
-    def plan(self, node_id: NodeId, last_principal_node_id: NodeId) -> Tuple[Policy, Powers, AttainmentProbabilities]:
+    def plan(self, node_id: NodeId, last_principal_node_id: NodeId = None) -> Tuple[Policy, Powers, AttainmentProbabilities]:
         """return a continuation policy for the given node, and the actual principal power evaluations and actual goal attainment probabilities resulting from it"""
         node = self.world_model.node_data(node_id)
         player = node.player
@@ -32,9 +32,9 @@ class CorrigibleAgent:
             return {}, { node_id: node.actual_power }, {}
 
         # so this node is not terminal
-        agent_policy = Policy()
-        actual_powers = Powers()
-        attainment_probabilities = AttainmentProbabilities()
+        agent_policy = {} # Policy()
+        actual_powers = {} # Powers()
+        attainment_probabilities = {} # AttainmentProbabilities()
         is_principal = player in self.world_model.principal_players
         if is_principal:
             last_principal_node_id = node_id
@@ -49,8 +49,9 @@ class CorrigibleAgent:
         if player == self.world_model.agent_player:
             # calculate and store Boltzmann local policy based on actual powers and durations:
             action_propensities = { 
-                np.exp(actual_powers[node.consequences[action]] 
-                       * np.exp(-self.power_discount_rate * node.durations[action]) 
+                action:
+                np.exp((actual_powers.get(node.consequences[action], None) or 1) 
+                       * np.exp(-self.power_discount_rate * node.durations.get(action, 1)) 
                        / self.boltzmann_temperature)
                 for action in actions }
             total_propensities = sum(action_propensities[action] for action in actions)
@@ -62,14 +63,14 @@ class CorrigibleAgent:
         # based on actual agent policy rather than principal's beliefs about agent policy
         # (hence using override_policy_belief=True, which works as the previous line has already stored 
         # the agent's continuation policy in the world model):
-        _, _, _, powers, attprobs = self.get_assumed_principal_policy_and_more(node_id, last_principal_node_id, override_policy_belief=True)
+        _, _, _, powers, attprobs = self.world_model.get_assumed_principal_policy_and_more(node_id, last_principal_node_id, override_policy_belief=True)
         actual_powers.update({ node_id: powers[node_id] })
         attainment_probabilities.update(attprobs)
 
         return agent_policy, actual_powers, attainment_probabilities
 
 if __name__ == "__main__":
-    from corrigibility.game import SimpleExample
+    from corrigibility.world_model import SimpleExample
     game = SimpleExample()
-    agent = CorrigibleAgent(world_model=game, agent_player="Cora", principal_players=["Prince"])
+    agent = CorrigibleAgent(world_model=game)
     print(agent.plan(game.initial_node))
